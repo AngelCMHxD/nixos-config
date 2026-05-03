b7a7eeea88ec25ecf2c01ab98f6bc1946100d179
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-05-02 20:12:33 -0500

    Add partition manager and zed settings

--- home.nix
+++ home.nix
@@ -53,11 +53,62 @@
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
-    zed-editor # TODO: Move settings to Home Manager
     neovim
+    kdePackages.partitionmanager
     python3
     ghostty # TODO: Move settings to Home Manager
   ];
+
+  programs.zed-editor = {
+    enable = true;
+
+    extensions = [
+      "catppuccin"
+      "catppuccin-icons"
+      "discord-presence"
+    ];
+
+    userSettings = {
+      cli_default_open_behavior = "new_window";
+      project_panel = {
+        dock = "left";
+        sort_mode = "directories_first";
+        hide_hidden = false;
+        hide_root = false;
+        indent_guides.show = "always";
+
+        git_status_indicator = true;
+        diagnostic_badges = false;
+        bold_folder_labels = true;
+        auto_fold_dirs = false;
+
+        indent_size = 15;
+
+        git_status = true;
+        folder_icons = true;
+        file_icons = true;
+        entry_spacing = "standard";
+      };
+
+      languages.Python.format_on_save = "off";
+      inlay_hints = {};
+
+      lsp = {
+        discord_presence = {
+          initialization_options = {
+            details = "Working on a {language:u} file";
+            state = ":P";
+          };
+        };
+      };
+
+      buffer_line_height.custom = 1.55;
+
+      tab_size = 4;
+      icon_theme = "Catppuccin Macchiato";
+      theme = "Catppuccin Macchiato";
+    };
+  };
 
   programs.zen-browser = {
     enable = true;
