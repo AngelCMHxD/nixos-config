e03d57b6d066e3ae573e7a115cf011798163ad07
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-05-03 20:38:37 -0500

    Setup starship prompt

--- home.nix
+++ home.nix
@@ -52,6 +52,7 @@
   programs.home-manager.enable = true;
   home.username = "angel";
   home.homeDirectory = "/home/angel";
+  home.shell.enableFishIntegration = true;
   home.stateVersion = "25.11"; # This should match what is in configuration.nix's system.stateVersion.
 
   # Packages installed in the user profile (overrides system profile).
@@ -126,6 +127,44 @@
       font-family = "JetBrainsMono Nerd Font";
       adjust-cursor-thickness = 3;
       quit-after-last-window-closed = false;
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    presets = ["nerd-font-symbols"];
+
+    settings = {
+      palette = "catppuccin-macchiato";
+
+      palettes.catppuccin-macchiato = {
+        rosewater = "#f4dbd6";
+        flamingo = "#f0c6c6";
+        pink = "#f5bde6";
+        mauve = "#c6a0f6";
+        red = "#ed8796";
+        maroon = "#ee99a0";
+        peach = "#f5a97f";
+        yellow = "#eed49f";
+        green = "#a6da95";
+        teal = "#8bd5ca";
+        sky = "#91d7e3";
+        sapphire = "#7dc4e4";
+        blue = "#8aadf4";
+        lavender = "#b7bdf8";
+        text = "#cad3f5";
+        subtext1 = "#b8c0e0";
+        subtext0 = "#a5adcb";
+        overlay2 = "#939ab7";
+        overlay1 = "#8087a2";
+        overlay0 = "#6e738d";
+        surface2 = "#5b6078";
+        surface1 = "#494d64";
+        surface0 = "#363a4f";
+        base = "#24273a";
+        mantle = "#1e2030";
+        crust = "#181926";
+      };
     };
   };
 
