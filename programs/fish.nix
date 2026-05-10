08313c31d910d9d6fb3881cbcfd0cba863abd2ca
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-05-10 16:47:02 -0500

    fish: add aliases for devtools

--- programs/fish.nix
+++ programs/fish.nix
@@ -7,6 +7,12 @@
       nixos-rebuild-switch = "sudo nixos-rebuild switch --flake /etc/nixos#ichigo";
       nixos-rebuild-boot = "sudo nixos-rebuild boot --flake /etc/nixos#ichigo";
       get-extension-id = "${pkgs.bash}/bin/bash ${./zen-browser/get-extension-id.sh}";
+
+      ls = "lsd";
+      ll = "lsd -l";
+      la = "lsd -a";
+      cat = "bat";
+      htop = "btop";
     };
   };
 }
