2f8c9c7873fe6f3a99594348637c21793f20f772
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-05-30 21:44:44 -0500

    more mangowm settings

--- programs/mangowm/default.nix
+++ programs/mangowm/default.nix
@@ -9,12 +9,20 @@
         settings = {
             xkb_rules_layout = "latam";
             trackpad_natural_scrolling = 1;
+            sloppyfocus = 0;
+            numlockon = 1;
 
             border_radius = 3;
             rootcolor = "0xc6a0f6ff";
             maximizescreencolor = "0xc6a0f6ff";
             focuscolor = "0xc6a0f6ff";
             bordercolor = "0x6e738dff";
+
+            animation_duration_move = 200;
+            animation_duration_open = 200;
+            animation_duration_tag = 200;
+            animation_duration_close = 200;
+            animation_duration_focus = 200;
 
             gappih = 4;
             gappiv = 4;
@@ -37,6 +45,11 @@
                 "id:9,layout_name:scroller"
             ];
 
+            gesturebind = [
+                "none,left,3,viewtoright_have_client"
+                "none,right,3,viewtoleft_have_client"
+            ];
+
            	bind = [
                 "SUPER+SHIFT,r,reload_config"
                 "SUPER,Return,spawn,ghostty"
@@ -54,6 +67,10 @@
                 "NONE,Print,spawn,noctalia msg screenshot-region"
                 "SUPER+SHIFT,s,spawn,noctalia msg screenshot-region"
                 "SHIFT,Print,spawn,noctalia msg screenshot-fullscreen"
+
+                # Resize windows
+                "SUPER,minus,resizewin,-10,0"
+                "SUPER,plus,resizewin,+10,0"
 
                 # Switch window focus
                 "SUPER,Left,focusdir,left"
